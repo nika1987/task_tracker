1779b029582d68295a117e919512307db67863ae
veronika <@Nika_Zolotova>
2024-04-18 00:46:16 +0400

    change employees_services.py

--- services/employees_services.py
+++ services/employees_services.py
@@ -32,7 +32,7 @@ class EmployeeService:
 
         async with db.begin():
             result = await db.execute(
-                self.model.employee.insert().values(new_employee)
+                self.model.insert().values(new_employee)
             )
             return bool(result.rowcount)
 
