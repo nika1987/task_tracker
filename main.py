d6e43506bfbb360bae4f24acff482c77b1e084c8
veronika <@Nika_Zolotova>
2024-05-20 00:34:25 +0400

    fix

--- main.py
+++ main.py
@@ -9,4 +9,4 @@ app.include_router(tasks_router.task_router)
 app.include_router(employees_router.employee_router)
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run('main:app', host='127.0.0.1', port=8004, reload=True)
+    uvicorn.run('main:app', host='127.0.0.1', port=8005, reload=True)
