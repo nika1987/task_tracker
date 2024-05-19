dc9a199d1b2f46ee8689f540fd23ccfa6162fc71
veronika <@Nika_Zolotova>
2024-05-20 00:21:41 +0400

    fix

--- src/dao/employees_dao.py
+++ src/dao/employees_dao.py
@@ -20,7 +20,6 @@ class EmployeeDao:
     def __init__(self) -> None:
         """Initialize the EmployeeDao class"""
 
-        self.employee_dao = EmployeeDao()
         self.model = Employee
         self.tasks = Task
 
